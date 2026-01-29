d5a6d09b1a5fc15e4cdc5a0f01b57f43d4d43a74
GG-00000 <ggutierrez48@cps.edu>
2026-01-29 20:53:36 +0000

    Number7

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -2,7 +2,15 @@
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
 
 # Don't call the function, you just need to define it.
-
+list[1,2,5,7,9,-1]
+def all_positives(variable):
+    for num in list:
+        if num < 0:
+            return True
+        else:
+            return False
+        
+print(all_positives())
 
 
 
--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,12 +1,35 @@
+def tea_order(customer_name, tea_type, *args, **kwargs):
+    print(customer_name, "ordered a" , tea_type, "tea")
+    for arg in args:
+        print("- Add", args)
+    for key, value in kwargs.items():
+        print(" - Add", key, ":", value)
+
+print(tea_order("Alice", "Chamomile"))
+print(tea_order("Bob", "Black", milk="Oat"))
+print(tea_order("Tony", "Black", milk="Oat", sweetener="honey"))
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
+def sum_squares(*args):
+    sum = 0
+    for num in args:
+        sum += num ** 2
+    return sum
 
+print(sum_squares(1,2,3))
 
 # Indefinite Arguments (*args) Practice #2
 # Create a function called absolute_sum, which takes any number of arguments, and returns the sum of their absolute values (that is, it takes the non-negative values and adds them together, in other words, considers them all - negative and positive - as positive).
+def absolute_sum(*args):
+    total = 0
+    for num in args:
+        total += abs(num)
+    return total
 
+print(absolute_sum(-1,-2,3))
 # Indefinite Arguments (*args) Practice #3
 # Create a function called personal_numbers that receives, as its first argument, a name, and then an indefinite number of values.
 
